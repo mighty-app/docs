c0887938e5687d07f027d4ed63e323527f12ac8e
Matt Ramotar <matt.ramotar@jhu.edu>
2021-04-22 10:32:42 -0400

    Add theme

---
+++ src/theme.js
@@ -0,0 +1,46 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    background: {
+      default: '#ffffff',
+      dark: '#201c2b',
+      paper: '#201c2b',
+      alt: '#222127',
+      'alt-2': '#2F2F37',
+      accent: '#782CE8',
+      'accent-alt': '#5C19C6',
+      'accent-alt-2': '#451094',
+    },
+    primary: {
+      main: '#9246FF',
+    },
+
+    purple: {
+      light: '#e2dbf0',
+      main: '#6441a4',
+      dark: '#201c2b',
+    },
+
+    gray: {
+      light: '#e5e3e8',
+      main: '#424242',
+      dark: '#232127',
+    },
+
+    fill: {
+      brand: '#9246ff',
+      info: '#1e69ff',
+      warn: '#fdca61',
+      alert: '#e81b16',
+      success: '#3ce3c9',
+    },
+
+    http: {
+      get: '#',
+    },
+  },
+});
+
+export default theme;
